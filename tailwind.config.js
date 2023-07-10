b6d5119887874433440cb1399c667a6a36b44e52
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2023-07-10 08:12:08 -0300

    Header da página principal(index.html).

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        botao: {
+          fundo: {
+            preto: "#111111",
+            hover: "#2E2E2E",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
